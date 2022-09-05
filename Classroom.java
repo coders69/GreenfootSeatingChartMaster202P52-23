7af7a23ae9a989a44841434cfb25d770a408ff21
pranavigpalli <105763873+pranavigpalli@users.noreply.github.com>
2022-09-05 11:16:13 -0700

    fixing merge conflicts

--- Classroom.java
+++ Classroom.java
@@ -135,6 +135,7 @@ public class Classroom extends World
         studentdesk19.setDeskGroup(8);
         studentdesk18.setDeskGroup(8);
         studentdesk20.setDeskGroup(8);
+        
     }
    
     
@@ -146,9 +147,9 @@ public class Classroom extends World
     private void prepare()
     {
         createDeskLayout();
-     // Each student needs to create their specific instance following the KilgoreTrout example.
-     // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
-     // and then clicking on the inspect text
+ // Each student needs to create their specific instance following the KilgoreTrout example.
+ // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
+ // and then clicking on the inspect text
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
@@ -209,6 +210,8 @@ public class Classroom extends World
         PranaviGollanapalli pranavigollanapalli = new PranaviGollanapalli();
         addObject(pranavigollanapalli,8,3);
         pranavigollanapalli.assignSeat();
+
+
     }
     
     public List<Student> getAllStudents(){
